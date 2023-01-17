92b9f7ac8080ea197e079d099a825920208defb0
rafadot1 <rafaelairesdemedeiros@outlook.com>
2023-01-16 21:14:57 -0300

    deploy

--- src/main/java/com/sorteiominecraft/Sorteiominecraft/SorteioMinecraftApplication.java
+++ src/main/java/com/sorteiominecraft/Sorteiominecraft/SorteioMinecraftApplication.java
@@ -3,6 +3,7 @@ package com.sorteiominecraft.Sorteiominecraft;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SorteioMinecraftApplication {
