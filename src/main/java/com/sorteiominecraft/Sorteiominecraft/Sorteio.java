366b1dfc12505f71a9c73b78619c2680731e5ffe
rafadot1 <rafaelairesdemedeiros@outlook.com>
2023-01-19 12:59:42 -0300

    alteração classes

--- src/main/java/com/sorteiominecraft/Sorteiominecraft/Sorteio.java
+++ src/main/java/com/sorteiominecraft/Sorteiominecraft/Sorteio.java
@@ -15,30 +15,23 @@ public class Sorteio {
         this.players.add("Dot");
         this.players.add("Tobias");
 
-        this.classes.add("Tolo");
         this.classes.add("Fazendeiro");
         this.classes.add("Rancheiro");
         this.classes.add("Minerador");
-        this.classes.add("Lenhador");
         this.classes.add("Cozinheiro");
         this.classes.add("Ferreiro");
         this.classes.add("Clérigo");
         this.classes.add("Mercante");
         this.classes.add("Explorador");
         this.classes.add("Guerreiro");
-        this.classes.add("Arqueiro");
-        this.classes.add("Gatuno");
         this.classes.add("Mestre das Feras");
 
-        this.origens.add("Humano");
         this.origens.add("Aviário");
         this.origens.add("Aracnídio");
         this.origens.add("Elitriano");
         this.origens.add("Shulk");
-        this.origens.add("Felino");
         this.origens.add("Enderiano");
         this.origens.add("Oceânico");
-        this.origens.add("Incandescente");
         this.origens.add("Vulto");
     }
 
